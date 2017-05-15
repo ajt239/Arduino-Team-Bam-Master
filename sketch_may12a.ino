cf48ded1704e1bc0db09159efc4ec23444523696
ajt239 <ajt239@nau.edu>
2017-05-15 14:48:41 -0700

    Arduino Sketch to use AT Commands with Bluetooth Module
    This Arduino Sketch is so that parents/guardians/ Go-Baby-Go car builders will be able to configure the HC-08 Bluetooth Module to their liking.

---
+++ sketch_may12a.ino
@@ -0,0 +1,28 @@
+#include <SoftwareSerial.h>
+
+//define pins and create serial connection for bluetooth adapter 
+//Pin 7 for TXD
+//pin 8 for RXD 
+SoftwareSerial bluetoothSerial(7,8);
+
+void setup() {
+  
+  //start serial connection to bluetooth module 
+  bluetoothSerial.begin(9600); 
+  
+  //start serial connection between arduino and computer 
+  Serial.begin(9600); 
+  Serial.println("Enter AT Commands for HC-08: ");
+}
+
+void loop() {
+  //read input from HC-08 and print to serial monitor 
+  if(bluetoothSerial.available()){
+    Serial.write(bluetoothSerial.read()); 
+  }
+
+  //read input from Serial monitor and send it to the HC-08
+  if(Serial.available()){
+    bluetoothSerial.write(Serial.read()); 
+  }
+}
